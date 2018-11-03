52dee6f192168d8ffcfc6b3923b89bfe2e8d789c
paulmolloy <molloyp1@tcd.ie>
2018-11-03 13:57:45 +0000

    Commented out installing drivers.

--- vmsetup.sh
+++ vmsetup.sh
@@ -4,8 +4,8 @@ sudo apt update
 sudo apt upgrade
 sudo apt-get install build-essential
 sudo apt-get install linux-image-extra-virtual
-wget http://us.download.nvidia.com/XFree86/Linux-x86_64/367.128/NVIDIA-Linux-x86_64-367.128.run
-sudo /bin/bash NVIDIA-Linux-x86_64-367.128.run
+#wget http://us.download.nvidia.com/XFree86/Linux-x86_64/367.128/NVIDIA-Linux-x86_64-367.128.run
+#sudo /bin/bash NVIDIA-Linux-x86_64-367.128.run
 wget https://bootstrap.pypa.io/3.2/get-pip.py
 chmod +x get-pip.py
 sudo apt-get install python
