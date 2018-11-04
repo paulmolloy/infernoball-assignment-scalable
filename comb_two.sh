ffa09d237934063a1bf2bcdbd000e43dbadccb92
paulmolloy <molloyp1@tcd.ie>
2018-11-04 22:45:57 +0000

    4american and combtwo

--- comb_two.sh
+++ comb_two.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #              ^^^^-- NOT /bin/sh
 path="$1"
-readarray -t a < "$path"   # read each line of file1 into an element of the array "a"
-readarray -t b < "$path"   # read each line of file2 into an element of the array "b"
+readarray -t a <"$path"   # read each line of file1 into an element of the array "a"
+readarray -t b <"$path"   # read each line of file2 into an element of the array "b"
 for itemA in "${a[@]}"; do
   for itemB in "${b[@]}"; do
     printf '%s%s\n' "$itemA" "$itemB"
