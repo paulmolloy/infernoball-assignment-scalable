1ed715d55ec3beb0b51f861d68e1b8e0d1120dbe
paulmolloy <molloyp1@tcd.ie>
2018-11-05 10:06:10 +0000

    pwdgen 5 n.

---
+++ gen_npwgen5l.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#              ^^^^-- NOT /bin/sh
+
+for i in {1..100}; do
+  uniq <(pwgen -0  5 -N 100000) | tr ' ' '\n' >> pwdgenLikely.txt
+
+done
+uniq pwdgenLikely.txt > pwdgen5n.txt
+rm pwdgenLikely.txt
