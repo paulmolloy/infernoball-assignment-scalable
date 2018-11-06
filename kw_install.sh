896bc30aa2fccd64727edf15233366e89618f1ac
paulmolloy <molloyp1@tcd.ie>
2018-11-06 09:11:53 +0000

    Add kwpprocessor install script.

---
+++ kw_install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/hashcat/kwprocessor.git
+cd kwprocessor
+make
+./kwp basechars/full.base keymaps/en-us.keymap routes/2-to-10-max-3-direction-changes.route  >> ../keyboard4kwp10.txt
+
